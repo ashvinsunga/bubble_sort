fa444fe8e8276565ec595a90e6ed9f172ccaeb8c
ashvinsunga <ashvinsunga@yahoo.com.ph>
2021-09-10 13:08:11 +0800

    Pseudocode added

---
+++ script.js
@@ -0,0 +1,6 @@
+// THE GOAL OF THIS FUNCTION
+// Pseudocode
+// Start looping from with a variable called i the end of the array towards the beginning
+// Start an inner loop with a variable called j from the beginning until -1
+// if arr[j] is greater than arr[j+1], swap those two values!
+// Return the sorted array
